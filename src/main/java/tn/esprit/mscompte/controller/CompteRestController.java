f9afd7b1fe06d9efb17b2b3aac71dc1a51eda970
GHARBI Abdelhafidh <130412760+AbdelhafidhGHARBI@users.noreply.github.com>
2025-05-04 19:23:34 +0100

    ✨ Amélioration du CompteRestController : ajout/modification des endpoints ou logique de gestion

--- src/main/java/tn/esprit/mscompte/controller/CompteRestController.java
+++ src/main/java/tn/esprit/mscompte/controller/CompteRestController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/comptes")
+@RequestMapping("/comptes")
 @RequiredArgsConstructor
 @CrossOrigin
 public class CompteRestController {

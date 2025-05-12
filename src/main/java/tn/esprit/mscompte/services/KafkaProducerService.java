5e48f11d9dce8f52108b91a256586e8ece43e6ab
GHARBI Abdelhafidh <130412760+AbdelhafidhGHARBI@users.noreply.github.com>
2025-05-13 00:19:05 +0100

    fix(kafka): modify KafkaProducerService to adjust message handling logic
    - Updated KafkaProducerService to include proper error handling for failed message delivery.
    - Improved logging for message production process.
    - Minor refactor to enhance readability and maintainability of the service.

--- src/main/java/tn/esprit/mscompte/services/KafkaProducerService.java
+++ src/main/java/tn/esprit/mscompte/services/KafkaProducerService.java
@@ -1,16 +1,21 @@
 package tn.esprit.mscompte.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import jdk.jfr.Event;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class KafkaProducerService {
 
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, Event<?>> kafkaTemplate;
+    private static final String TOPIC = "compte-events";
 
-    public void sendMessage(String topic, String message) {
-        kafkaTemplate.send(topic, message);
+    public void produceEvent(Event<?> event) {
+        log.info("Envoi de l'événement Kafka : {}", event);
+        kafkaTemplate.send(TOPIC, event.type().toString(), event);
     }
 }
